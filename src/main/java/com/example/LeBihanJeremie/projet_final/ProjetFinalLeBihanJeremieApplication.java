61b083e036eac714e3260b14d835d54acd66b3e6
Jerem-LBN <lebihanjeremie2@gmail.com>
2024-05-03 09:50:19 +0200

    Initial_Push

---
+++ src/main/java/com/example/LeBihanJeremie/projet_final/ProjetFinalLeBihanJeremieApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeBihanJeremie.projet_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinalLeBihanJeremieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFinalLeBihanJeremieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeBihanJeremie/projet_final/ProjetFinalLeBihanJeremieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeBihanJeremie.projet_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFinalLeBihanJeremieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
